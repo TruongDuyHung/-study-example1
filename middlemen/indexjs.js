07428329bf80f8059b47fed1332e782ff422a631
rebelvn <61506538+rebelvn@users.noreply.github.com>
2020-03-11 13:42:18 +0700

    abcd

---
+++ middlemen/indexjs.js
@@ -0,0 +1,8 @@
+function nolove() {
+    document.getElementById('no').style.top= Math.floor(Math.random()*300)+'px';
+    document.getElementById('no').style.left= Math.floor(Math.random()*500)+'px';
+}
+function yeslove() {
+    alert('Say oh yeah');
+
+}
